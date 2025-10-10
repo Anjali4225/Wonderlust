e2b75aad3bd90401f0a78618302f6625c7a345de
Anjali Kumari <anjali04225@gmail.com>
2025-10-10 21:48:54 +0530

    first commit

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public"))); 
 
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
+
 const dbUrl = process.env.ATLASDB_URL;
 
 main().then(() => {
